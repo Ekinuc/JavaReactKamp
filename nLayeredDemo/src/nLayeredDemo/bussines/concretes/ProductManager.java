3708030d4e3f3b33eb7d89c46f8cdf723e070c82
MAHIR <MAHIR@DESKTOP-54J2TSM>
2021-05-06 12:51:46 +0300

    JavaCamp

---
+++ nLayeredDemo/src/nLayeredDemo/bussines/concretes/ProductManager.java
@@ -0,0 +1,37 @@
+package nLayeredDemo.bussines.concretes;
+
+import java.util.List;
+
+import nLayeredDemo.bussines.abstracts.ProductService;
+import nLayeredDemo.core.LoggerService;
+import nLayeredDemo.dataAccess.abstracts.ProductDao;
+import nLayeredDemo.entities.concrates.Product;
+
+
+public class ProductManager implements ProductService {
+	private ProductDao productDao;
+	private LoggerService loggerService;
+	
+	public ProductManager(ProductDao productDao, LoggerService loggerService) {
+		super();
+		this.productDao = productDao;
+		this.loggerService = loggerService;
+	}
+
+	@Override
+	public void add(Product product) {
+		if (product.getCategoryId() ==1) {
+			System.out.println("Bu kategoride ürün kabul edilmiyor");
+			return;
+		}
+	this.productDao.add(product);
+	this.loggerService.logToSystem("Ürün eklendi : " + product.getName());
+	}
+
+	@Override
+	public List<Product> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
