84336bb0b6ee479ce5f20fd9e93eed918efbdace
Ekinuc <77417466+Ekinuc@users.noreply.github.com>
2022-06-30 21:54:02 +0300

    Delete inheritance2 directory

--- inheritance2/src/inheritance2/CustomerManager.java
+++
@@ -1,9 +0,0 @@
-package inheritance2;
-
-public class CustomerManager {
-	public void add(Logger logger) {
-		//musteri ekleme kodlarý
-		System.out.println("Müþteri eklendi");
-		logger.log();
-	}
-}
