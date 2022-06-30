75d30acced42fedbfd875375811d4545a5a9c8e7
Ekinuc <77417466+Ekinuc@users.noreply.github.com>
2022-06-30 21:52:00 +0300

    Delete Odev2 directory

--- Odev2/src/InstructorManager.java
+++
@@ -1,6 +0,0 @@
-
-public class InstructorManager {
-	public void addCourse(Course course) {
-		System.out.println(course.getName()+"Kursu eklendi.");
-	}
-}
--- Odev2/src/StudentManager.java
+++
@@ -1,6 +0,0 @@
-
-public class StudentManager {
-	public void joinCourse(Course course) {
-		System.out.println(course.getName() +" Kursuna giriş yapıldı.");
-	}
-}
